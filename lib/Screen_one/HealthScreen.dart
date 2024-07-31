e70701b62968cc6e4702afb5a33aee2ee5370fab
waad <waadmahmoud2044@gmail.com>
2024-07-31 19:42:20 +0200

    app init

---
+++ lib/Screen_one/HealthScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HealthScreen extends StatelessWidget {
+  static const String routeName='health';
+  const HealthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
